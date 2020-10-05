0ae0c64c176c175df5b11dc10704a5a6a1fb7da1
musubi <1701307@st.asojuku.ac.jp>
2020-10-05 14:59:13 +0900

    スクリプトの追加 仮画像の追加

---
+++ Assets/Scripts/StartGoalManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGoalManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] startObjects;      // スタート地点のオブジェクト
+
+
+    public int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ステージに設置されてあるスタート地点を検索し、非アクティブ状態にする
+        startObjects = GameObject.FindGameObjectsWithTag("Start");
+        for(int i = 0; i < startObjects.Length; ++i)
+		{
+            startObjects[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown("joystick button 0"))
+		{
+			startObjects[count].SetActive(true);
+            count++;
+		}
+	}
+}
