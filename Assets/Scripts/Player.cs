48322fafe15bb9c816b29d970e53f42edf922bd4
musubi <1701307@st.asojuku.ac.jp>
2020-10-05 10:25:09 +0900

    基本プロジェクト追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //animator.SetFloat("Horizontal", x);
+        //animator.SetFloat("Vertical", z);
+
+        // 左スティックの情報をもとにキャラの向きの変更と移動
+        rb.velocity = new Vector3(x * 10, -3, z * 10);
+        if ((x != 0) || (z != 0))
+        {
+            Vector3 direction = new Vector3(x, 0, z);
+            transform.localRotation = Quaternion.LookRotation(direction);
+        }
+    }
+}
