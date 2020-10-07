39d00102f80f1255602fea4dc422b98aa524c0b8
musubi <1701307@st.asojuku.ac.jp>
2020-10-07 09:47:19 +0900

    プレハブの追加

---
+++ Assets/Scripts/GoalPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalPoint : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 rotateSpeed = new Vector3(0, 0.5f, 0);  // アイコンの回転速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 自身を回転させて目立たせる
+        transform.Rotate(rotateSpeed);
+    }
+}
