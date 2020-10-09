99ab2151c49d67b324c3f29a805c774a942e9402
inazuma <yuyuko9903@docomo.ne.jp>
2020-10-09 15:27:50 +0900

    カメラ作業中

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject mainCamera;
+    private Vector3 offset;                 //  キャラクターとカメラとのオフセット
+
+    private Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("unitychan");
+        offset = new Vector3(0, 1, -2);
+
+        playerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform _transform = player.transform;
+        //Vector3 forward = _transform.forward;
+
+        //Debug.Log(forward);
+
+        //transform.position = offset + player.transform.position;
+        //if(forward.z > 2)
+        //{
+
+        //}
+
+        //transform.rotation = player.transform.rotation;
+
+        transform.position += _transform.position - playerPos;
+        playerPos = _transform.position;
+    }
+}
