694291166e755e8568723e14c1e7157338f24295
inazuma <yuyuko9903@docomo.ne.jp>
2020-10-19 13:01:33 +0900

    カメラが向いている方向にプレイヤーが移動するように

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,41 +7,38 @@ public class CameraController : MonoBehaviour
     private GameObject player;
     private GameObject mainCamera;
     private Vector3 offset;                 //  キャラクターとカメラとのオフセット
-
     private Vector3 playerPos;
+    private float rotateSpeed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         this.player = GameObject.Find("unitychan");
-        offset = new Vector3(0, 1, 2);
-        transform.position = player.transform.position + offset;
+        offset = new Vector3(0, 3, -3);
+        playerPos = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform _transform = player.transform;
-        Vector3 forward = _transform.forward;
+        transform.position += player.transform.position - playerPos;
+        playerPos = player.transform.position;
 
-        //playerPos = player.transform.position;
+        if(Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.X))
+        {
+            AngleUpdate();
+        }
+    }
 
-        //transform.position = offset + playerPos;
-
-        transform.rotation = player.transform.rotation;
-
-        //transform.RotateAround(playerPos,new Vector3(1,0,0),oldPPos.x - playerPos.x * Time.deltaTime);
-
-        //if(playerPos != oldPPos)
-        //{
-        //    transform.RotateAround(playerPos, new Vector3(0, 1, 0), oldPPos.x - playerPos.x);
-        //}
-
-        //Debug.Log(player.transform.forward);
-
-        //oldPPos = playerPos;
-
-        //transform.position += _transform.position - playerPos;
-        //playerPos = _transform.position;
+    void AngleUpdate()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            transform.RotateAround(playerPos, Vector3.up,rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.RotateAround(playerPos, Vector3.up, -rotateSpeed);
+        }
     }
 }
